ec98f472480ab6366f251df3c55f7e8734e61a80
AlexB <systemdevcloud@live.co.uk>
2015-03-02 20:44:14 +0000

    push to remote repo

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div id="header"</div>
+    </div>
+    
+    <div id="main">
+        <div id="navigation">
+            
+        </div>
+        
+        <div id="page">
+            <h2>Admin Menu  </h2>
+        </div>
+        
+    </div>
+    </body>
+    
+</html>
---
+++ public/manage_content.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php include "../includes/layouts/header.php"; ?>
+ <?php include "../includes/functions.php"; ?>   
+    <div id="main">
+        <div id="navigation">
+            
+        </div>
+        
+        <div id="page">
+            <h2>Admin Menu  </h2>
+            <p>Welcome to the admin area</p>
+            <ul>
+                <li><a href="manage_content.php">Manage Website Content</a></li>
+                <li><a href="manage_admins.php">Manage Admins</a></li>
+                <li><a href="logout.php">Logout</a></li>
+                
+            </ul>
+        </div>
+    </div>
+<?php include "../includes/layouts/footer.php"; ?>
+
