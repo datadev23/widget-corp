df96dba4f158483121afe413d23dbe3e2a749354
AlexB <systemdevcloud@live.co.uk>
2015-03-04 14:50:41 +0000

    refactoring pages and subjects

--- includes/functions.php
+++ includes/functions.php
@@ -7,3 +7,44 @@
  */
 
 ?>
+
+
+<?php
+
+function find_all_subjects()
+{
+    global $connection;
+  $query  = "SELECT * ";
+	$query .= "FROM subjects ";
+	$query .= "WHERE visible = 1 ";
+	$query .= "ORDER BY position ASC";
+	$subjectset= mysqli_query($connection, $query);
+	// Test if there was a query error
+        
+        if (!$subjectset) {
+		die("Database query failed.");
+	}  
+        
+        return $subjectset;
+}
+
+
+function find_subject_by_id($subject_id)
+{
+    
+    global $connection;
+        $query  = "SELECT * ";
+	$query .= "FROM pages ";
+	$query .= "WHERE visible = 1 ";
+        $query .= "AND subject_id = {$subject_id} ";
+	$query .= "ORDER BY position ASC";
+	$pageset = mysqli_query($connection, $query);
+	// Test if there was a query error
+	if (!$pageset) {
+		die("Database query failed.");
+	}
+
+    return $pageset;
+}
+
+?>
