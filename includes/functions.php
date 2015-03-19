38b951c181de05e84a87d80affb3e7750a45aaa6
AlexB <systemdevcloud@live.co.uk>
2015-03-19 18:14:18 +0000

    add a new subject

--- includes/functions.php
+++ includes/functions.php
@@ -139,5 +139,16 @@ function confirm_query($result_set) {
 	}
 
 
+function redirect_to($new_location)
+{
+ header('Location: '.$new_location);
+ exit;
+}
 
+function mysql_clean($string)
+{
+     global $connection;
+    $escaped_string = mysqli_real_escape_string($connection, $string);
+    return $escaped_string;
+}
 ?>
---
+++ public/create_subject.php
@@ -0,0 +1,68 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<?php include "../includes/dbconnection.php";?>
+ <?php include "../includes/functions.php"; ?> 
+
+
+<?php
+
+if(isset($_POST["submit"])) {
+    
+    // get the data from the form for processing
+    
+    $menu_name = $_POST["menu_name"];
+    $position = $_POST["position"];
+    $visible = $_POST["visible"];
+    
+    echo $menu_name;
+    
+    // process the form
+    // make the insert 
+    
+    // clean data 
+    $menu_name = mysql_clean($menu_name);
+    
+    // insert the data from the form into the database
+    $query = "INSERT INTO subjects (";
+    $query .= "menu_name, position, visible";
+    $query .= ") VALUES (";
+    $query .= " '{$menu_name}', {$position}, {$visible}";
+    $query .= ")";
+    
+    // result
+   
+  
+ 
+    $result = mysqli_query($connection, $query) or die("failed result");
+    
+    // if result is succesful redirect to the manage_content.php
+ 
+    if ($result)
+    {
+        
+       redirect_to("manage_content.php");
+    }
+ 
+}
+else
+{
+    redirect_to("new_subject.php");
+}
+
+?>
+
+
+
+<?php
+global $connection;
+if (isset($connection))
+{
+    mysqli_close($connection);
+}
+?>
