5ad325532c929ba4d3b2d02077e3f2b4719a4003
AlexB <systemdevcloud@live.co.uk>
2015-03-20 16:04:47 +0000

    display the fields that can be edited

---
+++ public/edit_subject.php
@@ -0,0 +1,112 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<?php require_once "../includes/session.php";?>
+<?php include "../includes/dbconnection.php";?>
+ <?php include "../includes/functions.php"; ?> 
+ <?php require_once  "../includes/validation_functions.php"; ?> 
+<?php
+
+$subject_set = find_all_subjects();
+$subject_count = mysqli_num_rows($subject_set);
+?>
+
+<?php include "../includes/layouts/header.php"; ?>
+
+<?php
+if (isset($_GET["subject"]))
+{
+
+$current_subject =  find_subject_by_id($_GET["subject"]);
+$current_page = null;
+
+
+
+}
+elseif(isset($_GET['pages']))
+{
+
+$current_subject = null;
+ $current_page =  find_page_by_id($_GET["pages"]);
+}
+else
+{
+$current_page = null;    
+$current_subject = null;
+}
+?>
+  
+    <div id="main">
+        <div id="navigation">
+          
+          <?php  echo navigation($current_subject, $current_page); ?>
+            
+        </div>
+        
+        <div id="page">
+       <?php  
+        echo message();
+       
+         ?>   
+       <?php $errors = errors(); ?>
+       <?php echo form_errors($errors); ?>
+       <h2>Edit Subject</h2>
+     
+		<form action="create_subject.php" method="post">
+		  <p>Menu name:
+		    <input type="text" name="menu_name" value="<?php echo $current_subject["menu_name"] ;?>" />
+		  </p>
+		  <p>Position:
+		    <select name="position">
+                         
+                        <?php
+			$subject_set = find_all_subjects();
+$subject_count = mysqli_num_rows($subject_set);	
+for ($count =1; $count < $subject_count; $count++)
+{
+echo "<option value=\"{$count}\"";
+if ($current_subject["position"] == $count) {
+
+    echo " selected";
+}
+echo ">{$count}</option>";
+}
+echo $subject_count;
+?>
+                        
+		    </select>
+		  </p>
+		  <p>Visible:
+                    <input type="radio" name="visible" value="0" <?php if($current_subject["visible"] == 0) {echo "checked";}?> /> No
+		    &nbsp;
+                    <input type="radio" name="visible" value="1" <?php if($current_subject["visible"] == 1) {echo "checked";}?> /> Yes
+		  </p>
+		  <input type="submit" name="submit" value="Create Subject" />
+		</form>
+       
+        
+        </div>
+    </div>
+
+<?php // release the data
+ mysqli_free_result($subject_set);
+?>
+
+
+<?php include "../includes/layouts/footer.php"; ?>
+
+<?php 
+// close the connection
+
+/*
+global $connection;
+if (isset($connection))
+{
+    mysqli_close($connection);
+}
+*/    
+?>
